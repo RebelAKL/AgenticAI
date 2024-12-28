5e34f9d927d61d9148521bd06cd827e2019843bf
RebelAKL <rebel7soul@duck.com>
2024-12-28 17:31:07 +0530

    playground

---
+++ playground.py
@@ -0,0 +1,8 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+import os
+import openai
+from dotenv import load_dotenv
+load_dotenv()
